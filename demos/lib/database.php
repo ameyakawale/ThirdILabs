d45554db617d12ae4134fd53e5f89a4c77567e23
Sushil Muzumdar <sushil.muzumdar@the3i.com>
2018-03-08 23:43:44 -0500

    DB connection changes
    Changes to connect to Google Cloud SQL

--- demos/lib/database.php
+++ demos/lib/database.php
@@ -8,19 +8,19 @@
 
 function db_connecti() {
 
-	//$servername = "";
-	$servername = "45.63.12.123";
+	$servername = "";
+	//$servername = "45.63.12.123";
 	$username = "thirdi";
 	$password = "Welcome20";
 	$schema = "labsDB";
-	//$socket = "geometric-hull-152616:us-east1:thirdi-labs-db";
+	$socket = "geometric-hull-152616:us-east1:thirdi-labs-db";
 	
 	//print "SERVER:".$servername;
 	//print "USER:".$username;
 	//print "PASSWORD:".$password;
 	
-	//$result = mysqli_connect($servername, $username, $password, "", "", $socket);
-	$result = mysqli_connect($servername, $username, $password);
+	$result = mysqli_connect($servername, $username, $password, "", "", $socket);
+	//$result = mysqli_connect($servername, $username, $password);
 	
 	if (!$result)
 		return false;
